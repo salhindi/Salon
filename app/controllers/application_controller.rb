1d98c4b49bbf780c356ff3e6913ea9e85da6e0ec
salhindi <calisaraalhindi@gmail.com>
2021-07-14 09:49:38 -0700

    remove instance variables from partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in?
 
-    # before_action :require_login
-
     def current_user
         @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
